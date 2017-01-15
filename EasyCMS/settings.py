10b979b6ab603363c07a24daabdc1597d7c916f6
ProJie <1737723363@qq.com>
2017-01-16 00:06:41 +0800

    初始化项目,设置数据库

--- EasyCMS/settings.py
+++ EasyCMS/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'EasyCMS.wsgi.application'
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME'  : os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE'  : 'django.db.backends.mysql',
+		'NAME'    : 'easycms',  # 你的数据库名称
+		'USER'    : 'root',  # 你的数据库用户名
+		'PASSWORD': 'root',  # 你的数据库密码
+		'HOST'    : '127.0.0.1',  # 你的数据库主机，留空默认为localhost
+		'PORT'    : '3306',  # 你的数据库端口
 	}
 }
 
